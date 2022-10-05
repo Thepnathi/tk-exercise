e9e7226207310fa2fd6707f6b3143b922242e4fe
Thepnathi <thepnathi@hotmail.com>
2022-10-05 12:58:23 +0100

    initialise react app

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function App() {
+
+  React.useEffect(() => {
+    fetch('http://localhost:8000/api/artist/artist/')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
